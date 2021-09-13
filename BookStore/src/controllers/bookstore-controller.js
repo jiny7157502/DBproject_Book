3450f1c5bd62bafe98cbe2ff18d118e1db3a9066
LAPTOP-JMJ6A32F\pc <jiny7157502@naver.com>
2021-09-13 21:21:59 +0900

    로그인 화면(bootstrap)

--- BookStore/src/controllers/bookstore-controller.js
+++ BookStore/src/controllers/bookstore-controller.js
@@ -2,7 +2,7 @@ const BookstoreService = require('../services/bookstore-service');
 
 exports.getBookStore = async (req, res, next) => {
     try {
-        return res.render('index', { title : 'Express'});
+        return res.render('login', { title : 'Express'});
     } catch (err) {
         return res.status(500).json(err)
     }
